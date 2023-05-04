9a4753535a05e08c654b04f703c6ad742960f985
UsamaSaleem14 <usamasaleem0148@gmail.com>
2023-05-05 02:44:16 +0500

    Bug fix

--- pages/PythPrices/PythCard.tsx
+++ pages/PythPrices/PythCard.tsx
@@ -16,7 +16,7 @@ const PythCard = ({ image1, image2, name, price }: PythCardProps) => {
         <Image className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 w-16 h-16 -mt-4 -ml-8" src={image2} alt="Image 2" />
       </div>
       <div className="px-6 py-4">
-        <div className="font-bold text-4xl mb-2 text-zinc-600">{name.toLocaleUpperCase()}</div>
+        <div className="font-bold text-4xl mb-2 text-zinc-600">{name?.toLocaleUpperCase()}</div>
         <p className="text-gray-200 text-4xl">{price}</p>
       </div>
     </div>
